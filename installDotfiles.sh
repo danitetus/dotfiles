291fa0ef95260ef5628b099b059f5dc5127f10f6
Daniel Cañada Garcia <danitetus@gmail.com>
2020-12-09 00:03:21 +0100

    Added install script

---
+++ installDotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing .zshrc"
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+echo "Installing git configuration"
+rm $HOME/.gitconfig 2>/dev/null
+rm $HOME/.gitignore 2>/dev/null
+
+ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/git/.gotignore $HOME/.gitignore
+
+# ZSH THEME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/cdimascio/lambda-zsh-theme.git /tmp/lambda
+cp /tmp/lambda/cdimascio-lambda.zsh-theme $HOME/.oh-my-zsh/themes/
