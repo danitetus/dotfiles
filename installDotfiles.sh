b85489b0aa52a42b126d3e907ec48c94c377eed2
Daniel Cañada Garcia <danitetus@gmail.com>
2020-12-15 23:57:46 +0100

    Added backup/restore for gnome-terminal profiles

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -15,3 +15,6 @@ ln -s $HOME/.dotfiles/git/.gotignore $HOME/.gitignore
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/cdimascio/lambda-zsh-theme.git /tmp/lambda
 cp /tmp/lambda/cdimascio-lambda.zsh-theme $HOME/.oh-my-zsh/themes/
+
+# Gnome terminal
+dconf load /org/gnome/terminal/legacy/profiles:/ < $HOME/.dotfiles/gnome-terminal/gnome-terminal-profiles.dconf
