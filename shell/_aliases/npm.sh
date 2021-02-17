81f83238d2b62254d3e750080d8134982a890134
Daniel Cañada Garcia <danitetus@gmail.com>
2021-02-18 00:29:53 +0100

    Added alias for npm run

--- shell/_aliases/npm.sh
+++ shell/_aliases/npm.sh
@@ -1,3 +1,4 @@
 alias npi="npm install --save --no-progress --skip-integrity-check"
 alias npd="npm install --save-dev --no-progress --skip-integrity-check"
 alias npc="npm ci --no-progress --skip-integrity-check"
+alias npr="npm run"
